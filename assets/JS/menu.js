76f5e36bc104fc26638d7e800af65e79bc07e8df
AndrewRls <andrl57adf@gmail.com>
2026-06-07 18:43:06 -0300

    fix: tentando arrumar o menu

--- assets/JS/menu.js
+++ assets/JS/menu.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector(".menu__responsivo");
     const menuLinks = document.getElementById("menu__links");
-
-    // Verifica se os elementos existem na tela para evitar erros no console
     if (menuBtn && menuLinks) {
         menuBtn.addEventListener("click", function () {
             menuLinks.classList.toggle("active");
